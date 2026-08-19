f04c5ba5393cb42a9392b07ee7f877c5811066ed
Jonathan Zheng <jonathanzhengjiahao@gmail.com>
2026-08-19 23:06:13 +0800

    Update name from Duke to SevenSix

---
+++ src/main/java/SevenSix.java
@@ -0,0 +1,18 @@
+/**
+ * Starts the SevenSix application.
+ */
+public class SevenSix {
+    public static void main(String[] args) {
+        String banner = """
+                 _____                      _____ _      
+                / ____|                    / ____(_)     
+               | (___   _____   _____ _ __ | (___  ___  __
+                \\___ \\ / _ \\ \\ / / _ \\ '_ \\ \\___ \\| \\ \\/ /
+                ____) |  __/\\ V /|  __/ | | |____) | |>  <
+               |_____/ \\___| \\_/  \\___|_| |_|_____/|_/_/\\_\\
+               """;
+
+        System.out.println(banner);
+        System.out.println("Welcome to SevenSix!");
+    }
+}
