02bca7c1c53b37e69e897109be907c69aa359b6e
Jonathan Zheng <jonathanzhengjiahao@gmail.com>
2026-08-28 15:00:23 +0800

    Align Gradle and JUnit with duke package

--- src/test/java/duke/DateTimeParserTest.java
+++ src/test/java/duke/DateTimeParserTest.java
@@ -1,3 +1,5 @@
+package duke;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
